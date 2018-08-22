88d046b2fa205bf81813cdcc238134a2694838a0
Nathan Dabu <nathan.dabu@ucf.edu>
2018-08-22 14:43:53 -0400

    more initial setup; use babel ES6 instead of coffeescript

--- src/creator.js
+++ src/creator.js
@@ -1,1 +1,46 @@
 // creator js!
+const SortItOut = angular.module("SortItOutController", ['ngMaterial', 'ngMessages']);
+
+SortItOut.config( ($mdThemingProvider) =>
+	$mdThemingProvider.theme('toolbar-dark', 'default').primaryPalette('indigo').dark()
+);
+
+SortItOut.controller("SortItOutController", ($scope) => {
+	$scope.initNewWidget = (widget) => {
+		$scope.title = "My Sort-It-Out Widget";
+		$scope.$apply();
+	};
+
+	$scope.initExistingWidget = (title, widget, qset) => {
+		$scope.title = title;
+		$scope.qset = qset;
+	};
+
+	$scope.onSaveClicked = () => {
+		Materia.CreatorCore.save($scope.title, $scope.qset);
+	};
+
+	$scope.onQuestionImportComplete = (items) => {
+		// TODO
+		return true;
+	};
+
+	$scope.onSaveComplete = () => {
+		// TODO
+		return true;
+	};
+});
+
+/* TODO
+SortItOut.factory("Resource", () => {
+	{
+		buildQset: ( ... ) => {
+			// TODO
+		},
+
+		processQsetItem: () => {
+			// TODO
+		}
+	}
+})
+*/
