28ffb8e57cf04cbc2191a2c9274e689261df17ae
Nathan Dabu <nathan.dabu@ucf.edu>
2018-08-24 17:22:01 -0400

    finish basic functionality of creator

--- src/creator.js
+++ src/creator.js
@@ -1,73 +1,95 @@
-console.log("creator js");
-const SortItOut = angular.module("SortItOutCreator", ['ngMaterial', 'ngMessages']);
+const SortItOut = angular.module("SortItOutCreator", ['ngMaterial', 'ngMessages', 'ngSanitize']);
 
 SortItOut.config( ($mdThemingProvider) =>
 	$mdThemingProvider.theme('toolbar-dark', 'default').primaryPalette('indigo').dark()
 );
 
-SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
+SortItOut.controller("SortItOutController", ($scope, $mdDialog, $sanitize) => {
 
 	$scope.MAX_ITEM_LENGTH = 30;
 	$scope.MAX_NUM_BUCKETS = 4;
 
 	$scope.buckets = [
 		{
-			name: "bucket 1",
+			name: "Sample Bucket",
 			items: [
-				{ text: "number 1" },
-				{ text: "number 2" },
-				{ text: "number 3" }
-			]
-		},
-		{
-			name: "bucket 2",
-			items: [
-				{ text: "number 4" },
-				{ text: "number 5" },
-				{ text: "number 6" }
-			]
-		},
-		{
-			name: "bucket 3 is has max len of 35 chars",
-			items: [
-				{ text: "number 7" },
-				{ text: "number 8" },
-				{ text: "number 9" },
-				{ text: "number 10" },
-				{ text: "number 11" }
+				{ text: "Sample Item" },
 			]
 		}
 	];
 	$scope.editBucketIndex = 0;
+	$scope.ready = false;
 
 	$scope.initNewWidget = (widget) => {
 		console.log("initNewWidget");
 		$scope.title = "My Sort-It-Out Widget";
+		$scope.ready = true;
 		$scope.$apply();
 	};
 
 	$scope.initExistingWidget = (title, widget, qset) => {
 		console.log("initExistingWidget");
 		$scope.title = title;
-		$scope.qset = qset;
+		console.log(qset);
+		$scope.buckets = generateBuckets(qset.items);
+		$scope.ready = true;
 		$scope.$apply();
 	};
+
+	const generateBuckets = (qsetItems) => {
+		let buckets = [];
+		let bucketNameMatching = {};
+		let numBuckets = 0;
+
+		for (let qsetItem of qsetItems) {
+			const bucketName = qsetItem.answers[0].text;
+			const item = qsetItem.questions[0];
+			if (bucketNameMatching[bucketName] == undefined) {
+				buckets.push({
+					name: bucketName,
+					items: []
+				});
+				bucketNameMatching[bucketName] = numBuckets++;
+			}
+			buckets[bucketNameMatching[bucketName]].items.push(item);
+		}
+		return buckets;
+	}
 
 	$scope.addItem = (bucketIndex) => {
 		$scope.buckets[bucketIndex].items.push( { text: "" } );
 	};
 
-	$scope.addBucket = () => {
-		$scope.buckets.push({
-			name: "",
-			items: [
-				{ text: "" }
-			]
+	$scope.removeItem = (bucketIndex, itemIndex) => {
+		$scope.buckets[bucketIndex].items.splice(itemIndex, 1);
+	};
+
+	$scope.showAddDialog = (ev) => {
+		$scope.createBucketName = "";
+		$mdDialog.show({
+			contentElement: "#create-dialog-container",
+			parent: angular.element(document.body),
+			targetEvent: ev,
+			clickOutsideToClose: true,
+			openFrom: ev.currentTarget,
+			closeTo: ev.currentTarget
 		});
 	};
 
+	$scope.createBucket = () => {
+		$scope.buckets.push({
+			name: $scope.createBucketName,
+			items: [{ text: "" }]
+		});
+		$mdDialog.hide();
+	}
+
 	$scope.canAddBucket = () => {
 		return $scope.buckets.length < $scope.MAX_NUM_BUCKETS;
+	};
+
+	$scope.canDeleteBucket = () => {
+		return $scope.buckets.length > 1;
 	};
 
 	$scope.validBucket = (bucketIndex) => {
@@ -81,7 +103,7 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 				return false;
 			}
 		}
-		return true;
+		return $scope.buckets[bucketIndex].items.length > 0; // TODO should we allow empty buckets?
 	};
 
 	const allUnique = () => {
@@ -115,11 +137,23 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 				return `bucket "${bucketName}" contains an invalid item`;
 			}
 		}
+
+		for (let bucket of $scope.buckets) {
+			if (!bucket.name || !bucket.name.length) {
+				return "all buckets must have names";
+			}
+		}
+
+		if (!$scope.title || !$scope.title.length) {
+			return "widget needs a valid title";
+		}
+
 		return false;
 	};
 
 	$scope.showEditDialog = (ev, bucketIndex) => {
 		$scope.editBucketIndex = bucketIndex;
+		$scope.editBucketName = $scope.buckets[bucketIndex].name;
 		$mdDialog.show({
 			contentElement: "#edit-dialog-container",
 			parent: angular.element(document.body),
@@ -130,6 +164,14 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 		});
 	};
 
+	$scope.updateName = () => {
+		// TODO should it validate the name before adding it?
+		$scope.buckets[$scope.editBucketIndex].name = $scope.editBucketName;
+		$mdDialog.hide();
+	};
+
+	$scope.hideDialog = () => $mdDialog.hide();
+
 	$scope.showConfirmDelete = (ev) => {
 		const confirm = $mdDialog.confirm()
 			.title("Are you sure you want to delete this bucket?")
@@ -139,15 +181,10 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 			.ok("Delete")
 			.cancel("Cancel");
 		$mdDialog.show(confirm).then(
-			() => deleteBucket(),
+			() => $scope.buckets.splice($scope.editBucketIndex, 1),
 			() => null
 		);
-	}
-
-	const deleteBucket = () => {
-		console.log("deleting bucket ", $scope.editBucketIndex);
-		$scope.buckets.splice($scope.editBucketIndex, 1);
-	}
+	};
 
 	$scope.onSaveClicked = () => {
 		console.log("onSaveClicked");
@@ -157,9 +194,31 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 			Materia.CreatorCore.cancelSave(saveError);
 		}
 		else {
-			Materia.CreatorCore.save($scope.title, $scope.qset);
+			const qset = generateQset();
+			Materia.CreatorCore.save($scope.title, qset);
 		}
 	};
+
+	const generateQset = () => {
+		let qset = { items: [] };
+
+		for (let bucket of $scope.buckets) {
+			const bucketName = $sanitize(bucket.name);
+			bucket.items.forEach( (item) => {
+				const text = $sanitize(item.text);
+				qset.items.push({
+					materiaType: "question",
+					id: null,
+					type: "QA",
+					options: {}, // TODO add 'description'
+					questions: [{ text }],
+					answers: [{ value: 100, text: bucketName }]
+				});
+			});
+		}
+		console.log(qset);
+		return qset;
+	}
 
 	$scope.onQuestionImportComplete = (items) => {
 		console.log("onQuestionImportComplete");
@@ -175,17 +234,3 @@ SortItOut.controller("SortItOutController", ($scope, $mdDialog) => {
 
 	Materia.CreatorCore.start($scope);
 });
-
-/* TODO
-SortItOut.factory("Resource", () => {
-	{
-		buildQset: ( ... ) => {
-			// TODO
-		},
-
-		processQsetItem: () => {
-			// TODO
-		}
-	}
-})
-*/
