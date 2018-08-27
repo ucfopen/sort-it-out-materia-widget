688a026112b95a42cb2c13219d2e8986c4b8edb2
Nathan Dabu <nathan.dabu@ucf.edu>
2018-08-27 14:46:49 -0400

    start on widget player

--- src/player.js
+++ src/player.js
@@ -1,1 +1,27 @@
-// player.js!
+const SortItOut = angular.module("SortItOutEngine", []);
+
+SortItOut.controller("SortItOutEngineCtrl", ($scope) => {
+
+	$scope.start = (instance, qset, version) => {
+		console.log("qset: ", qset);
+		$scope.instance = instance;
+		$scope.folders = buildFolders(qset);
+		$scope.items = buildItems(qset);
+		$scope.$apply();
+	}
+
+	const buildFolders = qset => {
+		let folders = [];
+		console.log("buildFolders");
+		return folders;
+	}
+
+	const buildItems = qset => {
+		let items = [];
+		console.log("buildItems");
+		return items;
+	}
+
+	Materia.Engine.start($scope);
+});
+
