c6f39f3ae09fa2db80ecbbea8a0a753eedee7775
Nathan Dabu <nathan.dabu@ucf.edu>
2018-08-29 13:46:12 -0400

    added basic (non-drag) ability for items to be put into buckets

--- src/player.js
+++ src/player.js
@@ -2,6 +2,8 @@ const SortItOut = angular.module("SortItOutEngine", []);
 
 SortItOut.controller("SortItOutEngineCtrl", ($scope) => {
 
+	let selectedText;
+
 	$scope.start = (instance, qset, version) => {
 		console.log("qset: ", qset);
 		$scope.instance = instance;
@@ -36,6 +38,32 @@ SortItOut.controller("SortItOutEngineCtrl", ($scope) => {
 		console.log("\n\n\n\n");
 		console.log("desktopItems: ", $scope.desktopItems);
 		console.log("folders: ", $scope.folders);
+	}
+
+	$scope.selectItem = (e, text) => {
+		const target = $(e.currentTarget);
+		const alreadySelected = target.hasClass("selected");
+
+		$(".desktop-item.selected").removeClass("selected");
+		if (alreadySelected) {
+			selectedText = null;
+		} else {
+			target.addClass("selected");
+			selectedText = text;
+		}
+	}
+
+	$scope.selectFolder = index => {
+		if (selectedText) {
+			$scope.folders[index].items.push(selectedText);
+			$scope.desktopItems = $scope.desktopItems.filter(
+				text => text != selectedText
+			);
+			$(".desktop-item.selected").removeClass("selected");
+			selectedText = null;
+		} else {
+			console.log("this would open the folder");
+		}
 	}
 
 	Materia.Engine.start($scope);
