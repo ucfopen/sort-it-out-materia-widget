4994ec45de34abaa5c4737d668c9bb4066bf4d31
Nathan Dabu <nathan.dabu@ucf.edu>
2018-08-29 13:21:22 -0400

    continue work on player, added dependencies for drag&drop

--- src/player.js
+++ src/player.js
@@ -6,20 +6,36 @@ SortItOut.controller("SortItOutEngineCtrl", ($scope) => {
 		console.log("qset: ", qset);
 		$scope.instance = instance;
 		$scope.folders = buildFolders(qset);
-		$scope.items = buildItems(qset);
+		$scope.desktopItems = buildItems(qset);
 		$scope.$apply();
 	}
 
 	const buildFolders = qset => {
 		let folders = [];
-		console.log("buildFolders");
+		let seenFolders = {};
+		qset.items.forEach( item => {
+			const text = item.answers[0].text;
+			if (!seenFolders[text]) {
+				seenFolders[text] = true;
+				folders.push({
+					text,
+					items: []
+				});
+			}
+		});
 		return folders;
 	}
 
 	const buildItems = qset => {
-		let items = [];
-		console.log("buildItems");
-		return items;
+		return qset.items.map( item =>
+			item.questions[0].text
+		);
+	}
+
+	$scope.logEverything = () => {
+		console.log("\n\n\n\n");
+		console.log("desktopItems: ", $scope.desktopItems);
+		console.log("folders: ", $scope.folders);
 	}
 
 	Materia.Engine.start($scope);
