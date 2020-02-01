c75a633e473423836343ff2a69e44bcbb357e984
Ian Turgeon <iturgeon@gmail.com>
2020-01-31 23:12:19 -0500

    fix auto babel polyfills and remove ie 11 support

--- webpack.config.js
+++ webpack.config.js
@@ -41,9 +41,11 @@ const babelLoaderWithPolyfillRule = {
 	use: {
 		loader: 'babel-loader',
 		options: {
+			ignore:['node_modules'],
 			presets: [
 				['@babel/preset-env', {
-					targets: { browsers: [">0.25%, ie > 11, not op_mini all"]},
+					targets: { browsers: [">0.25%, not ie 11, not op_mini all"]},
+					useBuiltIns: 'usage',
 					debug: true
 				}]
 			]
